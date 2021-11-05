fbbce6c21bdd932bbe86d3aa57ab2860e47e0b0c
rodriguezlucas1984 <rodriguezlucasjavier1984@gmail.com>
2021-11-05 10:48:51 -0300

    Despliegue de app React

--- app.js
+++ app.js
@@ -5,27 +5,23 @@ const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT;
 
-//hbs
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-
 //Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", { nombre: "Lucas Rodriguez", titulo: "Curso de Node" });
-});
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    nombre: "Lucas Rodriguez",
-    titulo: "Curso de Node",
-  });
-});
-app.get("/generic", (req, res) => {
-  res.render("generic", { nombre: "Lucas Rodriguez", titulo: "Curso de Node" });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", { nombre: "Lucas Rodriguez", titulo: "Curso de Node" });
+// });
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     nombre: "Lucas Rodriguez",
+//     titulo: "Curso de Node",
+//   });
+// });
+// app.get("/generic", (req, res) => {
+//   res.render("generic", { nombre: "Lucas Rodriguez", titulo: "Curso de Node" });
+// });
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("*", (req, res) => {
